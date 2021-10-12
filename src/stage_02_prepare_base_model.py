3f351ea05eb2217fac91728256367190cebeeca9
Bibek Rauth <rauth.bibek007@gmail.com>
2021-10-13 00:19:52 +0530

    Stage 02 completed and custom model saved

--- src/stage_02_prepare_base_model.py
+++ src/stage_02_prepare_base_model.py
@@ -1,11 +1,12 @@
 from os import read
 from src.utils.all_utils import read_yaml, create_dirs
-from src.utils.models import get_VGG16_model
+from src.utils.models import get_VGG16_model, prepare_model
 import argparse
 import pandas as pd
 import os
 from tqdm import tqdm
 import logging
+import io
 
 logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
 log_dir = "logs"
@@ -27,7 +28,28 @@ def base_model(config_path, param_path):
     create_dirs([base_model_dir_path])
     base_model_path = os.path.join(base_model_dir_path, base_model_name)
     input_shape = params['IMAGE_SIZE']
-    model = get_VGG16_model(input_shape=input_shape, model_path=base_model_dir_path)
+    learning_rate = params['LEARNING_RATE']
+    classes = params['N_CLASS']
+
+    model = get_VGG16_model(input_shape=input_shape, model_path=base_model_path)
+
+    full_model = prepare_model(model, 
+    classes, 
+    freeze_all=True, 
+    freeze_till=None,
+    learning_rate=learning_rate)
+
+    custom_model_name = artifacts['CUSTOM_MODEL_NAME']
+    custom_model_path = os.path.join(base_model_dir_path,custom_model_name)
+    def _log_model_summary(full_model):
+        with io.StringIO() as stream:
+            full_model.summary(print_fn=lambda x: stream.write(f"{x}\n"))
+            summary_str = stream.getvalue()
+        return summary_str
+
+    logging.info(f"full model summary: \n{_log_model_summary(full_model)}")
+
+    full_model.save(custom_model_path)
 
     
  
--- src/utils/models.py
+++ src/utils/models.py
@@ -1,5 +1,47 @@
+import tensorflow as tf
+import logging
 
 
-
+#preparing base model
 def get_VGG16_model(input_shape, model_path):
-    pass
+    model = tf.keras.applications.vgg16.VGG16(
+        input_shape=input_shape,
+        weights='imagenet',
+        include_top=False
+    )
+
+    model.save(model_path)
+    logging.info(f"VGG16 model saved at {model_path}")
+    return model
+
+#adding custom layers based on our requirements
+def prepare_model(model, CLASSES, freeze_all, freeze_till, learning_rate):
+    if freeze_all:
+        for layer in model.layers:
+            layer.trainable =  False
+        logging.info("Freeze all layers are done")
+    elif (freeze_till is not None) and (freeze_till>0):
+        for layer in model.layers[:-freeze_till]:
+            layer.trainable =  False
+        logging.info("Last {freeze_till} layer(s) are left for training and others are freeze")
+
+    flatten_in = tf.keras.layers.Flatten()(model.output)
+    predictions = tf.keras.layers.Dense(units=CLASSES,
+    activation='softmax')(flatten_in)
+
+    full_model = tf.keras.models.Model(
+        inputs=model.input,
+        outputs=predictions
+    )
+
+    full_model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=learning_rate),
+    loss=tf.keras.losses.CategoricalCrossentropy(),
+    metrics=['accuracy']
+    )
+
+    logging.info("custom model compiled and ready to be trained")
+    full_model.summary()
+    return full_model
+
+
+
