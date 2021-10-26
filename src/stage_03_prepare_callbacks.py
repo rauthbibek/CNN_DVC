d8876ec96d29c03c77003632ab537f96f4a826fd
Bibek Rauth <rauth.bibek007@gmail.com>
2021-10-26 09:30:42 +0530

    stage 3 completed

--- src/stage_03_prepare_callbacks.py
+++ src/stage_03_prepare_callbacks.py
@@ -13,9 +13,8 @@ log_dir = "logs"
 os.makedirs(log_dir, exist_ok=True)
 logging.basicConfig(filename=os.path.join(log_dir,"running_logs.log"), level=logging.INFO, format=logging_str, filemode="a")
 
-def prepare_callbacks(config_path, param_path):
+def prepare_callbacks(config_path):
     config = read_yaml(config_path)
-    params = read_yaml(param_path)
 
     artifacts = config['artifacts']
     artifacts_dir = artifacts['ARTIFACTS_DIR']
@@ -36,12 +35,11 @@ def prepare_callbacks(config_path, param_path):
 if __name__=='__main__':
     args = argparse.ArgumentParser()
     args.add_argument("--config", "-c", default="config/config.yaml")
-    args.add_argument("--params", "-p", default="params.yaml")
     parsed_args = args.parse_args()
 
     try:
         logging.info(">>>>>>>> Stage 3 started")
-        prepare_callbacks(config_path=parsed_args.config, param_path=parsed_args.params)
+        prepare_callbacks(config_path=parsed_args.config)
         logging.info("Stage 3 completed and callbacks saved as a binary file >>>>>>>>>>>>")
     except Exception as e:
         logging.exception(e)
--- src/utils/callbacks.py
+++ src/utils/callbacks.py
@@ -18,4 +18,13 @@ def create_and_save_tb_callback(callback_dir, tb_log_dir):
 
 
 def create_and_save_ckpt_callback(callback_dir, ckpt_dir):
-    pass
+    
+    ckpt_file = os.path.join(ckpt_dir, "ckpt_model.h5")
+    ckpt_callback = tf.keras.callbacks.ModelCheckpoint(
+        filepath=ckpt_file,
+        save_best_only=True
+    )
+
+    ckpt_callback_filepath = os.path.join(callback_dir, "checkpoint_cb.cb")
+    joblib.dump(ckpt_callback, ckpt_callback_filepath)
+    logging.info(f"Checkpoint callback saved at {ckpt_callback_filepath}")
